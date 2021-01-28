a79448da817591cbabc98ee360bdfc661a6d6572
Jenilee2108 <jenilee.mb@gmail.com>
2021-01-28 15:10:21 +0100

    ajout tout

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
-    echo("Hello world!")
+
+require("vendor/autoload.php");
+//$_SERVER["REQUEST_URI"]
+//JE VEUX SAVOIR QUAND JEE SUIS EN LIGNE SI CA ME RETOUTRNE MON URL OU PAS
+
+// SI je ne suis pas en ligne je veux charger mon .env 
+if($_SERVER["HTTP_HOST"] != "new-copain.herokuapp.com/"){
+$dotenv= Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+}
+//sinon je continue ici 
+
+$ary = array("token" => $_ENV["TOKEN"]);
+    $data= json_encode($ary);
+    //[{data:"Hello World"}]
+
+    echo($data)
 ?>
